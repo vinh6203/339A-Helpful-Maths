b5b011dcb5f12e1863ca9a3f3fe87c298b7b55b9
vinh6203 <100813986+vinh6203@users.noreply.github.com>
2022-05-06 17:20:24 +0700

    Create 266A Stones on the Table.cpp

---
+++ 266A Stones on the Table.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == s[i-1])
+        {
+            count++;
+        }
+        
+    }
+    cout << count;
+}
