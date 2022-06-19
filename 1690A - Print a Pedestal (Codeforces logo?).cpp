dc47d597836e900115b3fcc212eb71438a5db2e8
vinh6203 <100813986+vinh6203@users.noreply.github.com>
2022-06-19 19:38:30 +0700

    Create 1690A - Print a Pedestal (Codeforces logo?).cpp

---
+++ 1690A - Print a Pedestal (Codeforces logo?).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    int n;
+    cin >> t;
+    for (int w = 0; w < t; w++)
+    {
+        cin >> n;
+        if (n%3 == 0)
+        {
+            cout << n/3 << " " << n/3 + 1 << " " << n/3 - 1 << endl;
+        }
+        else if (n%3 == 1)
+        {
+            cout << n/3 << " " << n/3 + 2 << " " << n/3 - 1 << endl; 
+        }
+        else
+        {
+            cout << n/3 + 1 << " " << n/3 + 2 << " " << n/3 - 1 << endl;
+        }
+        
+    }
+    
+}
